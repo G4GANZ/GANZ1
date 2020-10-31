d4753e4bde6ac6d0f3b80354549ed6f3fb14143b
G4GANZ <72672655+G4GANZ@users.noreply.github.com>
2020-10-31 15:39:08 +0700

    Add files via upload

---
+++ gimang.py
@@ -0,0 +1,13 @@
+def main():
+    time.sleep(1)
+    os.system ('clear')
+    print '[96m'
+    os.system ('figlet SELAMAT DATANG di Tools kami')
+    print'[92m================================='
+    print'[97m Author : [96mGANZ X D1k0T '
+    print'[97m Team   : [96mIndonesia Black Hat '
+    print'[97m Collab  : GANZ X D1K0T '
+    print'[92m================================='
+    print'[92m++++++++++++ [97mM E N U [92m++++++++++++'
+    
+    
